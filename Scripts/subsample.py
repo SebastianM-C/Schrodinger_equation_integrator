dc57dd68c5eae3656ee8013c651bea8eb34641e5
sebastian <m.c.sebastian95@gmail.com>
2016-09-25 01:27:31 +0300

    Add subsampling factor as command line argument

--- Scripts/subsample.py
+++ Scripts/subsample.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python
 
 import os
+import sys
 from shutil import copy2
 
 os.chdir("../Bin")
 #if os.path.isfile("old-output.dat") == False:
 copy2("output.dat", "old-output.dat")
 
+if len(sys.argv) == 2 and sys.argv[1] > 0:
+	factor = argv[1]
+else:
+	factor = 2
 f = open("output.dat","r+")
 lines = f.readlines()
 f.seek(0)
 lineno = 0
 for i in lines:
 	lineno = lineno + 1
-	if lineno%4 == False:
+	if lineno % factor == False:
 		f.write(i)
 	if i == '\n':
 		f.write(i)
